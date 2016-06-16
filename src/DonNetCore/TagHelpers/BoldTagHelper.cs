52e8505caa47706410e76b6dc93079de8eb7eae6
Miles <miles@miniasp.com>
2016-06-16 15:44:07 +0800

    製作BoldTagHelper

---
+++ src/DonNetCore/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DonNetCore.TagHelpers
+{
+
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("bold")]//指定bold tag可以用
+    [HtmlTargetElement(Attributes ="bold")]//指定用在attribute，但是tag就會失敗    
+    public class BoldTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("bold");
+            output.PreContent.SetHtmlContent("<strong>");
+            output.PostContent.SetHtmlContent("</strong>");
+        }
+    }
+}
