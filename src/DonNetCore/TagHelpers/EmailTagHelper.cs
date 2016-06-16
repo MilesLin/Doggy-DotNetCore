4be04da545ea8e2975a7fb36cf5a66236752b65f
Miles <miles@miniasp.com>
2016-06-16 15:23:23 +0800

    email tag改寫成async

--- src/DonNetCore/TagHelpers/EmailTagHelper.cs
+++ src/DonNetCore/TagHelpers/EmailTagHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading;
 
 namespace DonNetCore.TagHelpers
 {
@@ -17,15 +18,14 @@ namespace DonNetCore.TagHelpers
         // Pascal case gets translated into lower-kebab-case.
         public string MailTo { get; set; }
 
-        public override void Process(TagHelperContext context, TagHelperOutput output)
+        public override async void Process(TagHelperContext context, TagHelperOutput output)
         {
             // 取代email tag
-            output.TagName = "a";
-
-            var address = MailTo + "@" + EmailDomain;
-            output.Attributes.SetAttribute("href", "mailto:" + address);
-            //output.Content.SetContent(address);
-
+            output.TagName = "a";            
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
         }
     }
 }
